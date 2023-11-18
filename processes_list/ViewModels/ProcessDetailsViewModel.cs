a8f4f7fc0a0124dcaafc2afc02154fe7d9c02fe7
FilipSzweda <filip.szweda1@gmail.com>
2023-11-18 16:27:14 +0100

    Set priority

--- processes_list/ViewModels/ProcessDetailsViewModel.cs
+++ processes_list/ViewModels/ProcessDetailsViewModel.cs
@@ -3,7 +3,9 @@ using Prism.Mvvm;
 using processes_list.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace processes_list.ViewModels
@@ -11,7 +13,9 @@ namespace processes_list.ViewModels
     public class ProcessDetailsViewModel : BindableBase
     {
         public ICommand KillCommand { get; private set; }
-        
+        public ICommand SetPriorityCommand { get; private set; }
+        public List<ProcessPriorityClass> AccessiblePriorities { get; } = Enum.GetValues(typeof(ProcessPriorityClass)).Cast<ProcessPriorityClass>().ToList();
+
         public bool IsSelectedProcess => SelectedProcess != null;
         private ProcessModel _selectedProcess;
         public ProcessModel SelectedProcess
@@ -26,9 +30,17 @@ namespace processes_list.ViewModels
             }
         }
 
+        private ProcessPriorityClass _selectedPriority;
+        public ProcessPriorityClass SelectedPriority
+        {
+            get => _selectedPriority;
+            set => SetProperty(ref _selectedPriority, value);
+        }
+
         public ProcessDetailsViewModel()
         {
             KillCommand = new DelegateCommand(Kill);
+            SetPriorityCommand = new DelegateCommand(SetPriority);
         }
 
         public void Update(ProcessModel process)
@@ -41,6 +53,20 @@ namespace processes_list.ViewModels
         {
             System.Diagnostics.Debug.WriteLine($"[DEBUG] Killed process {SelectedProcess.Name} with Id: {SelectedProcess.Id}");
             SelectedProcess.Process.Kill();
+        }
+
+        public void SetPriority()
+        {
+            try
+            {
+                SelectedProcess.Process.PriorityClass = SelectedPriority;
+                SelectedProcess.Priority = SelectedPriority;
+                SelectedProcess.PriorityString = SelectedProcess.Priority.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
