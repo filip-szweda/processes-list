a42f7d47326551c136d2a35016f983b7abae9007
FilipSzweda <filip.szweda1@gmail.com>
2023-11-18 13:51:07 +0100

    Setup new view

---
+++ processes_list/Views/ProcessDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace processes_list.Views
+{
+    /// <summary>
+    /// Interaction logic for ProcessDetailsView
+    /// </summary>
+    public partial class ProcessDetailsView : UserControl
+    {
+        public ProcessDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
