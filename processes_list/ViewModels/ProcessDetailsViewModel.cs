ebba237c3dc496c6d876f08347334e35e52b9259
FilipSzweda <filip.szweda1@gmail.com>
2023-11-18 15:59:08 +0100

    Implement kill

--- processes_list/ViewModels/ProcessDetailsViewModel.cs
+++ processes_list/ViewModels/ProcessDetailsViewModel.cs
@@ -4,11 +4,14 @@ using processes_list.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace processes_list.ViewModels
 {
     public class ProcessDetailsViewModel : BindableBase
     {
+        public ICommand KillCommand { get; private set; }
+        
         public bool IsSelectedProcess => SelectedProcess != null;
         private ProcessModel _selectedProcess;
         public ProcessModel SelectedProcess
@@ -25,13 +28,19 @@ namespace processes_list.ViewModels
 
         public ProcessDetailsViewModel()
         {
-
+            KillCommand = new DelegateCommand(Kill);
         }
 
         public void Update(ProcessModel process)
         {
             SelectedProcess = process;
             RaisePropertyChanged(nameof(IsSelectedProcess));
+        }
+
+        public void Kill()
+        {
+            System.Diagnostics.Debug.WriteLine($"[DEBUG] Killed process {SelectedProcess.Name} with Id: {SelectedProcess.Id}");
+            SelectedProcess.Process.Kill();
         }
     }
 }
